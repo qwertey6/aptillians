6c083aab73940133852cdad289ecea873bceff65
Andrew Benson <adbenson@stanford.edu>
2022-05-07 10:32:55 -0700

    Rename statspage.js -> info.js to evade adblockers

--- aptillians/frontend/public/js/info.js
+++ aptillians/frontend/public/js/info.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('send-button');
     function logSubmit(event) {
-        location.href = './fight.html?address=TODO!!&aptillianidTODO'
+        location.href = './fight.html?address=TODO&aptillianid=TODO'
     }
     button.addEventListener('click', logSubmit);
 
