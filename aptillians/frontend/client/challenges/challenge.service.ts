4a6eecac5e5412c15d51076ef3bab276de5e3468
magnum6actual <wayne.culbreth@gmail.com>
2022-05-07 11:12:25 -0700

    Finished out aptos client

--- aptillians/frontend/client/challenges/challenge.service.ts
+++ aptillians/frontend/client/challenges/challenge.service.ts
@@ -1,4 +1,4 @@
-import { AptosAccount, Types } from 'aptos';
+import { AptosAccount, Types, HexString } from 'aptos';
 import { AptillianIdentifierDto } from '../aptillians/aptillian.dto';
 import { AptosService } from '../aptos/aptos.service';
 import { AptillianId } from '../aptillians/types';
@@ -13,18 +13,36 @@ export class ChallengeService {
   async accept(player: AptosAccount, aptillianID: AptillianId): Promise<void> {
     const payload: Types.TransactionPayload = {
       type: 'script_function_payload',
-      function: `${this.aptosService.programAddress}::Aptillian::accept`,
+      function: `${this.aptosService.programAddress}::Aptillian::accept_entry`,
       type_arguments: [],
       arguments: [aptillianID],
     };
     await this.aptosService.signAndSubmitTxn(player, payload);
   }
 
-  async reject(): Promise<void> {
-    throw new Error('TODO!!');
+  async reject(player: AptosAccount, aptillianID: AptillianId): Promise<void> {
+    const payload: Types.TransactionPayload = {
+      type: 'script_function_payload',
+      function: `${this.aptosService.programAddress}::Aptillian::reject_entry`,
+      type_arguments: [],
+      arguments: [aptillianID],
+    };
+    await this.aptosService.signAndSubmitTxn(player, payload);
   }
 
-  async challenge(identifier: AptillianIdentifierDto): Promise<string> {
-    throw new Error('TODO!!');
+  //this had a string return - what were we expecting to return here?
+  async challenge(
+    player: AptosAccount,
+    challenger_aptillian_id: AptillianId,
+    target_address: HexString,
+    target_aptillian_id: AptillianId,
+  ): Promise<void> {
+    const payload: Types.TransactionPayload = {
+      type: 'script_function_payload',
+      function: `${this.aptosService.programAddress}::Aptillian::challenge_entry`,
+      type_arguments: [],
+      arguments: [challenger_aptillian_id, target_address, target_aptillian_id],
+    };
+    await this.aptosService.signAndSubmitTxn(player, payload);
   }
 }
--- aptillians/frontend/client/fights/fight.service.ts
+++ aptillians/frontend/client/fights/fight.service.ts
@@ -1,6 +1,26 @@
+import { AptosAccount, Types, HexString } from 'aptos';
+import { AptillianIdentifierDto } from '../aptillians/aptillian.dto';
+import { AptosService } from '../aptos/aptos.service';
+import { AptillianId } from '../aptillians/types';
 
 export class FightService {
-  async makeTurn(abilityId: number): Promise<void> {
-    throw new Error("TODO!!");
+  aptosService: AptosService;
+
+  constructor() {
+    this.aptosService = new AptosService();
+  }
+
+  async makeTurn(
+    player: AptosAccount,
+    aptillianId: AptillianId,
+    abilityId: number,
+  ): Promise<void> {
+    const payload: Types.TransactionPayload = {
+      type: 'script_function_payload',
+      function: `${this.aptosService.programAddress}::Aptillian::accept_entry`,
+      type_arguments: [],
+      arguments: [aptillianId, abilityId],
+    };
+    await this.aptosService.signAndSubmitTxn(player, payload);
   }
 }
